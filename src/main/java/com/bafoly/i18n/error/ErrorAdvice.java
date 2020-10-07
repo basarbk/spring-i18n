fa4fe68ff96e7f69c288e2ac447f99debd340a82
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-07 16:53:11 +0300

    custom i18n message source

--- src/main/java/com/bafoly/i18n/error/ErrorAdvice.java
+++ src/main/java/com/bafoly/i18n/error/ErrorAdvice.java
@@ -1,9 +1,12 @@
 package com.bafoly.i18n.error;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -14,11 +17,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorAdvice {
+
+  @Autowired
+  MessageSource messageSource;
   
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ApiError handleMethodArgNotValid(MethodArgumentNotValidException exception, HttpServletRequest request){
-    ApiError error = new ApiError(400, exception.getMessage(), request.getServletPath());
+  public ApiError handleMethodArgNotValid(MethodArgumentNotValidException exception, HttpServletRequest request, Locale locale){
+    String message = messageSource.getMessage("com.bafoly.validation", null, locale);
+    ApiError error = new ApiError(400, message, request.getServletPath());
     BindingResult bindingResult = exception.getBindingResult();
     Map<String, String> validationErrors = new HashMap<>();
     for(FieldError fieldError: bindingResult.getFieldErrors()){
